c5c5f4c2b33e4d168d7c9300b85dfe152e644f9c
Gabryel8818 <gabriel_melo8818@hotmail.com>
2023-09-06 02:44:51 -0300

    update magic-home-cli

--- functions/functions.sh
+++ functions/functions.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-HOST=$(yarn run magic-home scan --json | sed '1,2d'| sed '8d' | jq -r '.[].host')
-
+#HOST=$(magic-home discovery_json | sed '1,2d'| sed '8d' | jq -r '.[].host')
+HOST="10.0.1.119"
 
 hex2rgb() {
     hex=$(echo "${1^^}" | sed 's/#//g')
-
+    
     a=$(echo $hex | cut -c-2)
     b=$(echo $hex | cut -c3-4)
     c=$(echo $hex | cut -c5-6)
-
+    
     r=$(echo "ibase=16; $a" | bc)
     g=$(echo "ibase=16; $b" | bc)
     b=$(echo "ibase=16; $c" | bc)
-
+    
     echo $r $g $b
 }
 
 
 on_led(){
-     yarn run magic-home on $HOST
+    magic-home on $HOST
 }
 
 validate_color(){
@@ -31,14 +31,13 @@ validate_color(){
 }
 
 validate_state(){
-   STATE=$(yarn run magic-home state $HOST | sed '14d;1d;2d')
-   local status=$(echo "$STATE" | jq .on)
-
+    local status=$(magic-home query $HOST  | jq .on)
+    
     if [[ "$status" == "true" ]]
     then
         local color=$(hex2rgb $1)
         validate_color "$color" "$STATE"
-        yarn run magic-home color $HOST $color
+        magic-home setcolor $HOST $color
     else
         on_led;
     fi
